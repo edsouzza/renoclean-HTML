5b5285bd1443874562067c94d84eb129552d4705
Edi Aquino de Souza <edsouzza@hotmail.com>
2024-05-22 09:11:04 -0300

    feat: final of process responsive

--- js/Whats.js
+++
@@ -1,9 +0,0 @@
-const areaMsg   = document.querySelector('#areaDaMsg')
-const imgWhats  = document.querySelector('.imgWhats')
-
-const handleMouseEnter = () =>{
-    areaMsg.style.display = 'block'   
-}
-const handleMouseLeave = () =>{
-    areaMsg.style.display = 'none'   
-}
