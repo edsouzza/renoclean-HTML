13b5f13310376c7ca73aace7cfa60aa6c1340d66
Edi Aquino de Souza <edsouzza@hotmail.com>
2024-05-29 10:06:37 -0300

    update in form contatos about validation of fields before send

--- js/SendMail.js
+++ js/SendMail.js
@@ -1,8 +1,25 @@
 function sendMail() {
+  var name    = document.getElementById("name").value;
+  var email   = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  // Validação de preenchimento dos campos
+  if (!name || !email || !message) {
+    alert("Por favor, preencha todos os campos.");
+    return;
+  }
+
+  // Validação do formato do e-mail
+  var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(email)) {
+    alert("Por favor, insira um endereço de e-mail válido.");
+    return;
+  }
+
   var params = {
-    name    : document.getElementById("name").value,
-    email   : document.getElementById("email").value,
-    message : document.getElementById("message").value,
+    name    : name,
+    email   : email,
+    message : message,
   };
 
   const serviceID  = "service_l1wug9b";
@@ -10,13 +27,12 @@ function sendMail() {
 
   emailjs
     .send(serviceID, templateID, params)
-    .then(res=>{
-        document.getElementById("name").value    = "";
-        document.getElementById("email").value   = "";
-        document.getElementById("message").value = "";
-        console.log(res);
-        alert("Seu e-mail foi enviado com sucesso, retornaremos brevemente!")
+    .then((res) => {
+      document.getElementById("name").value    = "";
+      document.getElementById("email").value   = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Seu e-mail foi enviado com sucesso, retornaremos brevemente!");
     })
-    .catch(err=>console.log(err));
-
+    .catch((err) => console.log(err));
 }
